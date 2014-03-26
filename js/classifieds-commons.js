905ebd959af36e47fbddcc968891f98ff78c01ce
Emanuel Villarruel <villarruelemanuel@gmail.com>
2014-03-26 09:25:48 -0300

    mejoras de estilos

--- js/classifieds-commons.js
+++ js/classifieds-commons.js
@@ -1,4 +1,16 @@
 $('.ml-footer-control').on('click', function(){
     $(this).children('.icon').toggleClass('ch-icon-chevron-up').toggleClass('ch-icon-chevron-down');
     $('.ml-footer-access').toggleClass('ch-hide');
+    var addHeight = $('.ml-footer-access').outerHeight();
+
+    if($(this).children('.icon').is('.ch-icon-chevron-down')) {
+        $('.ml-content').css({
+            'padding-bottom': function(index, value) {
+              return parseFloat(value) + addHeight;
+            }
+        });
+    }else{
+        $('.ml-content').css('padding-bottom', '');
+    }
+    
 });
